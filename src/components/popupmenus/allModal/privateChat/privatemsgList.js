fc566f9fa5ce19420175558799446f089bc0f710
skw622 <skw622vlato@gmail.com>
2020-02-04 23:58:07 +0800

    fix bug

--- src/components/popupmenus/allModal/privateChat/privatemsgList.js
+++ src/components/popupmenus/allModal/privateChat/privatemsgList.js
@@ -28,6 +28,53 @@ export default class PrivateMsgList extends Component {
         return (
             <div className="scroll-privatechat privatechat-scroll">
                 {result}
+                <div className="col-12 m-b-sm d-flex align-items-start">
+                    <div className="profile-pics btn-group dropright">
+                        <span className="dropdown-open">
+                            <img src="/images/profile-pics/02.png" className="img-fluid rounded-circle b-user-pics mCS_img_loaded" alt="noimg" />
+                            <span className="arrow-left-caret"></span>
+                        </span>
+                    </div>
+                    <div className="chat-line lightgray-bg-line d-flex justify-content-between align-items-center m-full-block">
+                        <span className="d-flex m-left-block">
+                            <span className="btn-group">
+                                <span className="image-receive">
+                                    <img src="/images/receive-image.jpg" alt="noimg" />
+                                    <a href="#download" title="Download"><img src="/images/download-icon.png" alt="noimg" /></a>
+                                </span>
+                                <span className="image-receive">
+                                    <img src="/images/receive-image.jpg" alt="noimg" />
+                                    <a href="#download" title="Download"><img src="/images/download-icon.png" alt="noimg" /></a>
+                                </span>
+                            </span>
+                        </span>
+                        <span className="float-right font-12 m-right-block">05:00 PM</span>
+                    </div>
+                </div>
+                <div className="col-12 m-b-sm d-flex align-items-start">
+                    <div className="profile-pics btn-group dropright">
+                        <span className="dropdown-open">
+                            <img src="/images/profile-pics/02.png" className="img-fluid rounded-circle b-user-pics mCS_img_loaded" alt="noimg" />
+                            <span className="arrow-left-caret"></span>
+                        </span>
+                    </div>
+                    <div className="chat-line darkgray-bg-line align-items-center m-full-block">
+                        <div className="d-flex">
+                            <span className="btn-group">
+                                <span className="dropdown-open" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <b>Lín  Zhèng :</b>
+                                    <span className="arrow-left-caret"></span>
+                                    &nbsp;Request you to start a video call
+                                                    </span>
+                            </span>
+                        </div>
+                        <div className="request-block">
+                            <span className="mr-3 mt-2"><img src="/images/decline.png" className="img-fluid mr-2" title="Decline" alt="noimg" /><strong>Decline</strong></span>
+                            <span className="mr-3 mt-2"><img src="/images/accept.png" className="img-fluid mr-2" title="Accept" alt="noimg" /><strong>Accept</strong></span>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         )
     }
